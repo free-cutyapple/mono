4942ecb804e2cc055d7f3119231982f830178e42
Cutyapple <cutyapple@mathflat.com>
2022-09-07 12:45:58 +0900

    a update

--- packages/a/src/App.js
+++ packages/a/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>UPDATE !!!!! A/App.js</code> and save to reload.
+          Edit <code>AA UPDATE !!!!! A/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
