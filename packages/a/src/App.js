b7f3ffc90f95b0e634175f2521180204604026e1
Cutyapple <cutyapple@mathflat.com>
2022-09-07 14:16:02 +0900

    a

--- packages/a/src/App.js
+++ packages/a/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>AAAACC/App.js</code> and save to reload.
+          Edit <code>AAAACCa/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
