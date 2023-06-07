91b685e1546a673137803a730b312211b5c2ebd7
Cutyapple <cutyapple@mathflat.com>
2023-06-07 14:47:10 +0900

    a c

--- packages/a/src/App.js
+++ packages/a/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { log } from '../../c';
 
 function App() {
+  log();
+
   return (
     <div className="App">
       <header className="App-header">
