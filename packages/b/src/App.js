a77c8e0c0fe3d7c19c616b401cd68430ffe3ff92
Cutyapple <cutyapple@mathflat.com>
2023-06-07 15:00:34 +0900

    b v

--- packages/b/src/App.js
+++ packages/b/src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>B/bbbbbbbApp.js</code> and save to reload.
-        </p>
+        <p>b - vercel.json</p>
         <a
           className="App-link"
           href="https://reactjs.org"
