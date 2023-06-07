9d06c5cc1c9e60ac676c2ad5a5b23fa7f0e6662a
Cutyapple <cutyapple@mathflat.com>
2023-06-07 14:57:36 +0900

    a v

--- packages/a/src/App.js
+++ packages/a/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>ver. @free-cutyapple/c - log</p>
+        <p>ver. vercel.json</p>
         <a
           className="App-link"
           href="https://reactjs.org"
