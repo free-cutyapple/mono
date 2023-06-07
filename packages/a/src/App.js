8acd676b625c60276319e2e59ba0b5886b70f769
Cutyapple <cutyapple@mathflat.com>
2023-06-07 14:43:52 +0900

    a
    # ex) [Mono] - chore : .gkcommitmessage.txt 수정
    # [ ] 사이에 Package 이름을 적어줍니다. ex) Mono, Teacher, DesignSystem ...
    # - 뒤에 커밋 타입을 적어줍니다. ex) feat, chore, docs ...
    # : 뒤에 커밋의 주제를 적어줍니다. ex) 학습지 만들기 1단계 모의고사 수정

--- packages/a/src/App.js
+++ packages/a/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>AAAaaaaaaaaACCaac/App.js</code> and save to reload.
+          Edit <code>aa/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
